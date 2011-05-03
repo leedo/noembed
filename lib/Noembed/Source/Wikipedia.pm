742bb6da93dbaf5ad2733c04413bbc5346d17972
leedo <lee@laylward.com>
2011-05-03 16:57:17 -0500

    use Web::Scraper to filter wikipedia content

--- lib/Noembed/Source/Wikipedia.pm
+++ lib/Noembed/Source/Wikipedia.pm
@@ -1,17 +1,34 @@
 package Noembed::Source::Wikipedia;
 
 use Web::Scraper;
+use JSON;
 
 use parent 'Noembed::Source';
 
+sub new {
+  my ($class, %args) = @_;
+
+  my $self = {
+    scraper => scraper {
+      process "#firstHeading", title => 'TEXT';
+      process "#bodyContent p:first-child", summary => 'HTML';
+    }
+  };
+
+  bless $self, $class;
+}
+
 sub request_url {
   my ($self, $url) = @_;
+
   return $url;
 }
 
 sub filter {
   my ($self, $body) = @_;
-  return $body;
+
+  my $res = $self->{scraper}->scrape($body);
+  return encode_json $res;
 }
 
 sub patterns {
