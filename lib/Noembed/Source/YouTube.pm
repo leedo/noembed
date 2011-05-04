4646a533ae02d695ffd848fcb0673501d626963b
Lee Aylward <lee@laylward.com>
2011-05-03 21:43:33 -0500

    just use iframe embed for youtube

--- lib/Noembed/Source/YouTube.pm
+++ lib/Noembed/Source/YouTube.pm
@@ -1,28 +1,14 @@
 package Noembed::Source::YouTube;
 
-use Web::Scraper;
-
+use JSON;
 use parent 'Noembed::Source';
 
-my $re = qr{http://[^\.]+\.youtube\.com/watch\?v=(.+)}i;
-
-sub prepare_source {
-  my $self = shift;
-  $self->{scraper} = scraper {
-    process "title", title => 'TEXT';
-    process "body", html => sub {
-      $_[0]->tag('div');
-      $_[0]->attr('class', 'youtube');
-      $_[0]->as_HTML("");
-    };
-  };
-}
+my $re = qr{^http://[^\.]+\.youtube\.com/watch\?v=(.+)}i;
 
 sub request_url {
   my ($self, $req) = @_;
   my $url = $req->parameters->{url};
-  my ($id) = $url =~ $re;
-  return "http://www.youtube.com/embed/$id";
+  return "http://www.youtube.com/oembed/?url=$url";
 }
 
 sub matches {
@@ -32,6 +18,13 @@ sub matches {
 
 sub filter {
   my ($self, $body) = @_;
-  my $res = $self->{scraper}->scrape($body);
-  return $res;
+  my $data = decode_json $body;
+  my ($id) = $data->{html} =~ m{/v/([^\?]+)?};
+
+  my $width = $data->{width} || 640;
+  my $height = $data->{height} || 385;
+
+  $data->{html} = "<iframe type='text/html' width='$width' height='$height'"
+                . " src='http://www.youtube.com/embed/$id' frameborder=0></iframe>";
+  return $data;
 }
