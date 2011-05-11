90d4b247ee018d91efca9fb8d07488d1da51382b
Lee Aylward <lee@laylward.com>
2011-05-11 13:20:29 -0500

    strip whitepsace from GiantBomb title

--- lib/Noembed/Source/GiantBomb.pm
+++ lib/Noembed/Source/GiantBomb.pm
@@ -6,7 +6,7 @@ sub prepare_source {
   my $self = shift;
   $self->{re} = qr{https?://www\.giantbomb\.com/([^/]+)/\d+-\d+}i;
   $self->{video_re} = qr{"progressive_high":[^"]*"([^"]+)"}msi;
-  $self->{title_re} = qr{<title>(.*?) - Giant Bomb}msi;
+  $self->{title_re} = qr{<title>\s+(.*?) - Giant Bomb}msi;
 }
 
 sub provider_name { "GiantBomb" }
@@ -22,7 +22,7 @@ sub filter {
   my ($title) = $body =~ $self->{title_re};
   return +{
     title => $title,
-    html  => "<video controls width=640 height=360 src=\"$video\">$title</video>",
+    html  => "<video preload=\"none\" controls width=\"640\" height=\"360\" src=\"$video\">$title</video>",
   }
 }
 
