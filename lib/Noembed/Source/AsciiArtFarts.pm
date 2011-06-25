3b108a0d7b41689ce34f80b7bbf89250cb408ffa
Lee Aylward <lee@laylward.com>
2011-06-25 16:32:01 -0500

    added asciiartfarts.com source

---
+++ lib/Noembed/Source/AsciiArtFarts.pm
@@ -0,0 +1,31 @@
+package Noembed::Source::AsciiArtFarts;
+
+use Web::Scraper;
+use parent 'Noembed::Source';
+
+sub prepare_source {
+  my $self = shift;
+  $self->{re} = qr{http://www\.asciiartfarts\.com/[0-9]+\.html$}i;
+  $self->{scraper} = scraper {
+    process 'td[bgcolor="#000000"] font', html => 'RAW';
+    process 'h1', title => 'TEXT';
+  };
+}
+
+sub matches {
+  my ($self, $url) = @_;
+  return $url =~ $self->{re};
+}
+
+sub provider_name { "Ascii Art Farts" }
+
+sub filter {
+  my ($self, $body) = @_;
+  my $data = $self->{scraper}->scrape($body);
+  return +{
+    html => '<div class="ascii-fart-embed">'.$data->{html}.'</div>',
+    title => $data->{title},
+  };
+}
+
+1;
