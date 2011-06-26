30b0219d91aa7248b63910c36784d78c9dde091e
Lee Aylward <lee@laylward.com>
2011-06-25 21:38:30 -0500

    fix Giant Bomb video source

--- lib/Noembed/Source/GiantBomb.pm
+++ lib/Noembed/Source/GiantBomb.pm
@@ -1,25 +1,30 @@
 package Noembed::Source::GiantBomb;
 
+use HTML::Entities;
 use parent 'Noembed::Source';
 
 sub prepare_source {
   my $self = shift;
-  $self->{re} = qr{https?://www\.giantbomb\.com/([^/]+)/\d+-\d+}i;
-  $self->{video_re} = qr{"progressive_high":[^"]*"([^"]+)"}msi;
-  $self->{title_re} = qr{<title>\s+(.*?) - Giant Bomb}msi;
+  $self->{url_re} = qr{https?://www\.giantbomb\.com/([^/]+)/\d+-\d+}i;
+  $self->{attr_re} = qr{data-video="([^"]+)"};
+  $self->{video_re} = qr{"progressive_high":[^"]*"([^"]+)"};
+  $self->{title_re} = qr{<title>(.*?) - Giant Bomb};
 }
 
 sub provider_name { "GiantBomb" }
 
 sub matches {
   my ($self, $url) = @_;
-  $url =~ $self->{re};
+  $url =~ $self->{url_re};
 }
 
 sub filter {
   my ($self, $body ) = @_;
-  my ($video) = $body =~ $self->{video_re};
   my ($title) = $body =~ $self->{title_re};
+  my ($attr) = $body =~ $self->{attr_re};
+  $attr = decode_entities $attr;
+  my ($video) = $attr =~ $self->{video_re};
+
   return +{
     title => $title,
     html  => "<video preload=\"none\" controls width=\"640\" height=\"360\" src=\"$video\">$title</video>",
