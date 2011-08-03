b27cef959e5db02673beccdd6743112e7f15c100
Lee Aylward <lee@laylward.com>
2011-08-03 15:48:03 -0500

    style spotify embed

--- lib/Noembed/Source/Spotify.pm
+++ lib/Noembed/Source/Spotify.pm
@@ -1,13 +1,19 @@
 package Noembed::Source::Spotify;
 
 use HTML::Entities;
+use Web::Scraper;
 use parent 'Noembed::Source';
 
 sub prepare_source {
   my $self = shift;
-  $self->{url_re} = qr{https?://open\.spotify\.com/track/(\w{22})}i;
-  $self->{title_re} = qr{<title>(.*?) on Spotify</title>};
-  $self->{spotify_re} = qr{<meta property="og:audio" content="spotify:track:(\w{22})"};
+  $self->{re} = qr{https?://open\.spotify\.com/track/(\w{22})}i;
+  $self->{scraper} = scraper {
+    process 'title', title => 'TEXT';
+    process '#title', track => 'RAW';
+    process '#artist .meta-info', artist => 'RAW';
+    process '#album .meta-info', album => 'RAW';
+    process '#cover-art', coverart => '@src';
+  };
 }
 
 sub provider_name { "Spotify" }
@@ -19,13 +25,52 @@ sub matches {
 
 sub filter {
   my ($self, $body) = @_;
-  my ($title) = $body =~ $self->{title_re};
-  my ($spotify_link) = $body =~ $self->{spotify_re};
+
+  my $data = $self->{scraper}->scrape($body);
+  $data->{title} =~ s/ on Spotify//;
 
   return +{
-    title => $title,
-    html => "<a href=\"spotify:track:$spotify_link\">$title</a>",
+    title => $data->{title},
+    html  => '<div class="spotify-embed">'
+           .  '<img class="spotify-image" src="'.$data->{coverart}.'"/>'
+           .  '<span class="spotify-title">'.$data->{track}.'</span>'
+           .  '<span class="spotify-artist">'.$data->{artist}.'</span> from '
+           .  '<span class="spotify-album">'.$data->{album}.'</span>'
+           . '</div>'
   }
 }
 
+sub style {
+  $self->{style} ||= do {
+    local $/;
+    <DATA>;
+  };
+}
+
 1;
+
+__DATA__
+div.spotify-embed {
+  background: #373737;
+  color: #999;
+  overflow: hidden;
+}
+
+div.spotify-embed a {
+  color: #B3B3B3;
+  text-decoration: underline;
+}
+
+div.spotify-embed span.spotify-title {
+  font-size: 2em;
+  display: block;
+}
+div.spotify-embed span.spotify-title a {
+  color: #fff;
+  text-decoration: none;
+}
+
+div.spotify-embed img {
+  float: left;
+  margin-right: 10px;
+}
