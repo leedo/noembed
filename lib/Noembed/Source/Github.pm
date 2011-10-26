0e687db0593c76793f4debe4b2a1ffa6c63c6a2b
Lee Aylward <lee@laylward.com>
2011-10-26 00:49:42 -0500

    github shoturls can have underscores

--- lib/Noembed/Source/Github.pm
+++ lib/Noembed/Source/Github.pm
@@ -19,7 +19,7 @@ sub prepare_source {
   );
 }
 
-sub shorturls { 'http://git.io/[0-9a-zA-Z]' }
+sub shorturls { 'http://git.io/[_0-9a-zA-Z]' }
 sub patterns { 'https?://github.com/([^/]+)/([^/]+)/commit/(.+)' }
 sub provider_name { "Github Commit" }
 
