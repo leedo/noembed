1d7d24c8de6277f8b10bddb8085f4ba7f2c57986
Lee Aylward <lee@laylward.com>
2011-11-01 16:10:16 -0500

    fallback to text lexer if invalid one provided

--- lib/Noembed/Pygmentize.pm
+++ lib/Noembed/Pygmentize.pm
@@ -3,17 +3,51 @@ package Noembed::Pygmentize;
 use Carp;
 use IPC::Open3;
 use File::Which qw/which/;
+use List::Util qw/first/;
 use Symbol 'gensym'; 
 
 sub new {
   my ($class, %args) = @_;
 
-  bless {
+  my $self = bless {
     bin    => $args{bin}    || which("pygmentize") || "/usr/bin/pygmentize",
     lexer  => $args{lexer}  || "text",
     format => $args{format} || "html",
     options => $args{options} || "linenos=True",
   }, $class;
+
+  return $self;
+}
+
+sub has_lexer {
+  my ($self, $lexer) = @_;
+  return first { $_ eq $lexer } @{$self->lexers};
+}
+
+sub lexers {
+  my $self = shift;
+  $self->{lexers} ||= $self->build_lexers;
+}
+
+sub build_lexers {
+  my $self = shift;
+
+  my($wtr, $rdr, $err);
+  $err = gensym; #ugh
+
+  my $pid = open3($wtr, $rdr, $err,  $self->{bin}, "-L", "lexers");
+  close $wtr;
+  waitpid($pid, 0);
+
+  my @lexers;
+
+  while (my $line = <$rdr>) {
+    if ($line =~ /^* (.+):/) {
+      push @lexers, split ", ", $1;
+    }
+  }
+
+  return \@lexers;
 }
 
 sub colorize {
@@ -31,13 +65,21 @@ sub colorize {
   my $err = <$err>;
   my $out = <$rdr>;
 
-  carp $err if $err;
+  if ($err) {
+    carp $err if $err;
+    return $text;
+  }
   $out =~ s{</pre></div>\Z}{</pre>\n</div>};
   return $out;
 }
 
 sub command {
   my ($self, %opts) = @_;
+
+  if ($opts{lexer} and !$self->has_lexer($opts{lexer})) {
+    $opts{lexer} = "text";
+  }
+
   return (
     $self->{bin},
     '-l', $opts{lexer}   || $self->{lexer},
