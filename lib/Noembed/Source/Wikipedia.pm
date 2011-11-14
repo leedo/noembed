d4f09adf16ff0bdf397ba4087c41e154d2dd7b97
Lee Aylward <lee@laylward.com>
2011-11-14 15:30:36 -0600

    support wikipedia sub-sections

--- lib/Noembed/Source/Wikipedia.pm
+++ lib/Noembed/Source/Wikipedia.pm
@@ -1,69 +1,70 @@
 package Noembed::Source::Wikipedia;
 
-use Web::Scraper;
+use HTML::TreeBuilder::XPath;
 use List::MoreUtils qw/any/;
+use Text::MicroTemplate qw/encoded_string/;
 
 use parent 'Noembed::Source';
 
-sub prepare_source {
-  my $self = shift;
-
-  $self->{scraper} = scraper {
-    process "#firstHeading", title => 'TEXT';
-    process "#bodyContent", html => \&_extract_content;
-  };
-}
-
-sub patterns { 'http://[^\.]+\.wikipedia\.org/wiki/.*' }
+sub patterns { 'https?://[^\.]+\.wikipedia\.org/wiki/[^#]+(?:#(.+))?' }
 sub provider_name { "Wikipedia" }
 
 sub serialize {
-  my ($self, $body) = @_;
-  $self->{scraper}->scrape($body);
-}
+  my ($self, $body, $req) = @_;
+  my $tree = HTML::TreeBuilder::XPath->new;
+  $tree->parse($body);
+  my $title = $tree->findvalue('//h1[@id="firstHeading"]');
+  my $html;
 
-sub _extract_content {
-  my $el = shift;
+  if (my $img = $tree->findvalue('//div[@class="fullImageLink"]//img/@src')) {
+    $html = $self->render(image => $img);
+  }
 
-  my ($image) = $el->look_down(class => "fullImageLink");
-  if ($image) {
-    my ($img) = $image->find("img");
-    if ($img) {
-      my $src = $img->attr("src");
-      return "<a href=\"$src\" target=\"_blank\"><img src=\"$src\"></a>";
+  elsif (my $id = $req->captures->[0]) {
+    my $start = $tree->findnodes('//span[@id="'.$id.'"]/parent::*/following-sibling::*')->[0];
+    if ($start) {
+      $title .= ": " . $tree->findvalue('//span[@id="'.$id.'"]');
+      $html = $self->extract_text_content($start);
     }
   }
 
-  return _extract_text_content($el);
+  if (!$html) {
+    my $start = $tree->findnodes('//div[@class="mw-content-ltr"]/p')->[0];
+    $html = $self->extract_text_content($start);
+  }
+
+  return +{
+    title => $title,
+    html  => $html,
+  };
 }
 
-sub _extract_text_content {
-  my $el = shift;
+sub extract_text_content {
+  my ($self, $el) = @_;
   my $output;
-  my ($content) = $el->look_down(class => 'mw-content-ltr');
-  my @children = $content->content_list;
 
-  for my $child (@children) {
-    my $tag = $child->tag;
+  while ($el) {
+    my $tag = $el->tag;
 
     # stop once we hit the toc or a sub-head
-    last if $child->attr("id") eq "toc"
+    last if $el->attr("id") eq "toc"
          or $tag eq "h2";
 
     if (any {$tag eq $_} qw/p ul li/) {
 
       # fix the links
-      for my $a ($child->find("a")) {
+      for my $a ($el->find("a")) {
         my $href = $a->attr("href");
         $a->attr("target", "_blank");
         $a->attr("href", "http://www.wikipedia.org/$href");
       }
 
-      $output .= $child->as_HTML;
+      $output .= $el->as_HTML;
     }
+    $el = $el->right;
   }
 
-  return "<div class='wikipedia-embed'>$output</div>";
+  return $self->render(summary => encoded_string $output);
 }
 
 1;
