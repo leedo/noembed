4db8717b02d74b26a4f5e2e8091eda19f19509d3
Lee Aylward <lee@laylward.com>
2011-11-14 22:38:37 -0600

    use code ref to stop text extract loop

--- lib/Noembed/Source/Wikipedia.pm
+++ lib/Noembed/Source/Wikipedia.pm
@@ -27,13 +27,17 @@ sub serialize {
     if ($heading) {
       my $start = $heading->parent->right;
       $title .= ": " . $heading->as_text;
-      $html = $self->extract_text_content($start, $heading->parent->tag);
+      $html = $self->extract_text_content($start, sub {
+        $_[0]->tag eq $heading->parent->tag;
+      })
     }
   }
 
   if (!$html) {
     my $start = $root->look_down(class => "mw-content-ltr")->find("p");
-    $html = $self->extract_text_content($start, "table");
+    $html = $self->extract_text_content($start, sub {
+      $_[0]->tag eq "h2" or $_[0]->attr("class") eq "toc";
+    });
   }
 
   return +{
@@ -49,12 +53,10 @@ sub extract_text_content {
   $_->destroy for $el->parent->look_down(class => "editsection");
 
   while ($el) {
-    my $tag = $el->tag;
-
     # stop once we hit the stop tag
-    last if $tag eq $stop;
+    last if $stop->($el);
 
-    if (any {$tag eq $_} qw/p ul li h2 h3 h4 div/) {
+    if (any {$el->tag eq $_} qw/p ul li h2 h3 h4 div/) {
 
       # fix the links
       for my $a ($el->find("a")) {
