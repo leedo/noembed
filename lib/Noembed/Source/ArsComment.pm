362625ca75f37bf475e686e9cf3d23f503ff7af1
Lee Aylward <lee@laylward.com>
2011-11-14 23:40:44 -0600

    add Ars comment embed

---
+++ lib/Noembed/Source/ArsComment.pm
@@ -0,0 +1,29 @@
+package Noembed::Source::ArsComment;
+
+use HTML::TreeBuilder;
+
+use parent 'Noembed::Source';
+
+sub patterns {'http://arstechnica\.com/[^#]+#comment-(\d+)'}
+sub provider_name { "ArsTechnica" }
+
+sub serialize {
+  my ($self, $body, $req) = @_;
+  my $root = HTML::TreeBuilder->new_from_content($body)->elementify;
+  my $id = $req->captures->[0];
+  my $comment = $root->look_down("data-post-id" => $req->captures->[0]);
+
+  die "could not find comment" unless $comment;
+
+  my $title = $root->find("title")->as_text;
+  $comment->look_down(href => "#comment-$id")->attr("href", $req->url);
+  $comment->attr($_, undef) for qw/id class data-post-id/;
+  $comment->attr(class => "ars-comment-embed");
+
+  return +{
+    title => $title,
+    html => $comment->as_HTML,
+  }; 
+}
+
+1;
