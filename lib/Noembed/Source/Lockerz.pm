2a579668824414954baed4e7d8ddbefb7c443290
Lee Aylward <lee@laylward.com>
2012-02-05 19:00:34 -0600

    added Lockerz photos

---
+++ lib/Noembed/Source/Lockerz.pm
@@ -0,0 +1,23 @@
+package Noembed::Source::Lockerz;
+
+use parent 'Noembed::ImageSource';
+
+use Web::Scraper;
+
+sub prepare_source {
+  my $self = shift;
+  $self->{scraper} = scraper {
+    process '#photo', src => '@src';
+    process 'figcaption > p', title => 'TEXT';
+  };
+}
+
+sub image_data {
+  my ($self, $body) = @_;
+  $self->{scraper}->scrape($body);
+}
+
+sub provider_name { 'Lockerz' }
+sub patterns { 'http://lockerz\.com/s/\d+' }
+
+1;
