33b1fd4e7b70448dca1b9990bc3eafafc2e93cd1
Lee Aylward <lee@laylward.com>
2012-02-25 22:37:02 -0600

    force twiggy test server, escape uri

--- lib/Noembed/Test.pm
+++ lib/Noembed/Test.pm
@@ -4,6 +4,7 @@ use Carp;
 use HTTP::Request;
 use Test::More;
 use Plack::Test;
+use URI::Escape;
 use JSON;
 
 use base Exporter::;
@@ -11,6 +12,9 @@ use base Exporter::;
 
 sub test_embed {
   my %args = @_;
+
+  local $Plack::Test::Impl = 'Server';
+  local $ENV{PLACK_SERVER} = 'Twiggy';
 
   my $app    = delete $args{app} or croak "app is required";
   my $url    = delete $args{url} or croak "url is required";
@@ -20,7 +24,7 @@ sub test_embed {
     app => $app,
     client =>  sub {
       my $cb = shift;
-      my $req = HTTP::Request->new(GET => "/embed?url=$url");
+      my $req = HTTP::Request->new(GET => "/embed?url=".uri_escape($url));
       my $res = $cb->($req);
       my $input = decode_json $res->content;
       is_deeply $input, $output, $url;
