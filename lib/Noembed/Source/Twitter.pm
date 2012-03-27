5b03cb0b75042a8d22745f1959a138fb2d589d68
Lee Aylward <lee@laylward.com>
2012-03-27 11:42:01 -0500

    try using OAuth for twitter

--- lib/Noembed/Source/Twitter.pm
+++ lib/Noembed/Source/Twitter.pm
@@ -2,6 +2,8 @@ package Noembed::Source::Twitter;
 
 use JSON;
 use AnyEvent;
+use Net::OAuth::ProtectedResourceRequest;
+use Digest::SHA1;
 
 use parent 'Noembed::Source';
 
@@ -9,6 +11,39 @@ sub prepare_source {
   my $self = shift;
   $self->{name_re} = qr{(?:^|\W)(@[^\s:]+)};
   $self->{tweet_api} = "http://api.twitter.com/1/statuses/show/%s.json?include_entities=true";
+  $self->{credentials} = $self->read_credentials;
+}
+
+sub credentials {
+  my $self = shift;
+  my $file = Noembed::share_dir() . "/twitter_cred.json";
+  if (! -r $file) {
+    die "can not read twitter credentials: $file";
+  }
+  local $/;
+  open my $fh, "<", $file;
+  decode_json join "",  <$fh>;
+}
+
+sub oauth_url {
+  my ($self, $url) = @_;
+  my $cred = $self->credentials;
+
+  my $req = Net::OAuth::ProtectedResourceRequest->new(
+    version => '1.0',
+    timestamp => time,
+    nonce => Digest::SHA1::sha1_base64(time . $$ . rand),
+    signature_method => 'HMAC-SHA1',
+    request_url => $url,
+    request_method => "GET",  
+    extra_params => {include_entities => "true"},
+    consumer_key => $cred->{consumer_key},
+    consumer_secret => $cred->{consumer_secret},
+    token => $cred->{token},
+    token_secret => $cred->{token_secret},
+  );
+  $req->sign;
+  return $req->to_url;
 }
 
 sub shorturls { 'http://t\.co/[a-zA-Z0-9]+' }
@@ -17,7 +52,8 @@ sub provider_name { "Twitter" }
 
 sub build_url {
   my ($self, $req) = @_;
-  return sprintf $self->{tweet_api}, $req->captures->[0];
+  my $url = sprintf $self->{tweet_api}, $req->captures->[0];
+  return $self->oauth_url($url);
 }
 
 sub expand_entities {
