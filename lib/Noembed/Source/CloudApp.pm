721c74c18976dc53a4fb0ed50ac44ca44414965d
Lee Aylward <lee@laylward.com>
2012-04-06 23:21:47 -0500

    convert CloudApp to be an ImageSource

--- lib/Noembed/Source/CloudApp.pm
+++ lib/Noembed/Source/CloudApp.pm
@@ -2,21 +2,21 @@ package Noembed::Source::CloudApp;
 
 use Web::Scraper;
 
-use parent 'Noembed::Source';
+use parent 'Noembed::ImageSource';
 
 sub prepare_source {
   my $self = shift;
 
   $self->{scraper} = scraper {
-    process '#content', html => 'RAW';
-    process 'h2', title => 'TEXT';
+    process '#content img', src => '@src';
+    process '#content img', title => '@alt';
   };
 }
 
 sub provider_name { 'CloudApp' }
 sub patterns { 'http://cl\.ly/[0-9a-zA-Z]+/?$' }
 
-sub serialize {
+sub image_data {
   my ($self, $body) = @_;
   $self->{scraper}->scrape($body);
 }
