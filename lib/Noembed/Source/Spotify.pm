fbb73256a930caa10ff55ba97069a1030bd8b48a
Lee Aylward <lee@laylward.com>
2012-12-09 18:06:27 -0600

    get spotify artist name from musician URL
    fixes gh-13

--- lib/Noembed/Source/Spotify.pm
+++ lib/Noembed/Source/Spotify.pm
@@ -1,6 +1,7 @@
 package Noembed::Source::Spotify;
 
 use Web::Scraper;
+use Noembed::Util;
 use parent 'Noembed::Source';
 
 sub prepare_source {
@@ -9,18 +10,30 @@ sub prepare_source {
   $self->{scraper} = scraper {
     process 'meta[property="og:title"]', title => '@content';
     process 'meta[property="og:audio"]', link => '@content';
-    process '#artist .meta-info', artist => 'RAW';
+    process 'meta[property="music:musician"]', musician => '@content';
+  };
+  $self->{artist_scraper} = scraper {
+    process 'meta[property="og:title"]', name => '@content';
   };
 }
 
 sub patterns { 'https?://open\.spotify\.com/(track|album)/([0-9a-zA-Z]{22})' }
 sub provider_name { "Spotify" }
 
-sub serialize {
-  my ($self, $body, $req) = @_;
-
+sub post_download {
+  my ($self, $body, $req, $cb) = @_;
   my $data = $self->{scraper}->scrape($body);
-  $data->{artist} = clean_html($data->{artist});
+  Noembed::Util::http_get $data->{musician}, sub {
+    my ($body, $headers) = @_;
+    my $artist = $self->{artist_scraper}->scrape($body); 
+    $data->{artist} = clean_html($artist->{name});
+    $cb->($data);
+  };
+}
+
+sub serialize {
+  my ($self, $data, $req) = @_;
+
   $data->{type} = $req->captures->[0];
 
   return +{
