b1ebc7d907aa52bdca21d5e2c0e970271e3bd0ec
Lee Aylward <lee@laylward.com>
2013-03-06 23:20:06 -0600

    https soundcloud URLs

--- lib/Noembed/Source/SoundCloud.pm
+++ lib/Noembed/Source/SoundCloud.pm
@@ -3,7 +3,7 @@ package Noembed::Source::SoundCloud;
 use parent 'Noembed::oEmbedSource';
 
 sub provider_name {"SoundCloud"}
-sub patterns {'http://soundcloud.com/.*/.*'}
+sub patterns {'https?://soundcloud.com/.*/.*'}
 sub oembed_url {'http://soundcloud.com/oembed'}
 
 1;
