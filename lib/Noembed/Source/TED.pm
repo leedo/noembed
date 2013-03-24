09db8e81e22f4bd382a87cfb4ad0e26cba44afc4
Lee Aylward <lee@laylward.com>
2013-03-24 15:36:22 -0500

    fix TED embed

--- lib/Noembed/Source/TED.pm
+++ lib/Noembed/Source/TED.pm
@@ -8,7 +8,6 @@ sub prepare_source {
   my $self = shift;
 
   $self->{scraper} = scraper {
-    process 'meta[property="og:video"]', video => '@content';
     process 'meta[property="og:title"]', title => '@content';
     process 'meta[property="og:description"]', description => '@description';
     process 'meta[property="og:image"]', image => '@content';
@@ -16,12 +15,14 @@ sub prepare_source {
 }
 
 sub provider_name { "TED" }
-sub patterns { 'http://www\.ted\.com/talks/[^/]+\.html' }
+sub patterns { 'http://www\.ted\.com/talks/.+\.html' }
 
 sub serialize {
-  my ($self, $body) = @_;
+  my ($self, $body, $req) = @_;
 
   my $data = $self->{scraper}->scrape($body);
+  $data->{url} = $req->content_url;
+  $data->{url} =~ s/www\.ted\.com/embed\.ted\.com/;
 
   return {
     title => $data->{title},
