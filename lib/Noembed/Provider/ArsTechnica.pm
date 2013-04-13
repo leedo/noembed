430536d96f50bbb8be28e8bbed01667a936178eb
Lee Aylward <lee@laylward.com>
2013-04-13 10:39:40 -0500

    fix ars pattern to not include comments

--- lib/Noembed/Provider/ArsTechnica.pm
+++ lib/Noembed/Provider/ArsTechnica.pm
@@ -3,6 +3,6 @@ package Noembed::Provider::ArsTechnica;
 use parent 'Noembed::TwitterCardProvider';
 
 sub provider_name { "Ars Technica" }
-sub patterns { 'http://arstechnica\.com/[^/]+/\d+/\d+/[^/]+/?' }
+sub patterns { 'http://arstechnica\.com/[^/]+/\d+/\d+/[^/]+/?$' }
 
 1;
