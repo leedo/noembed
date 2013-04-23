58d793bc1b7a5a190fa7cecb8913c82d9bc05571
Lee Aylward <lee@laylward.com>
2013-04-23 09:48:27 -0500

    move Noembed::Imager::dimensions into Worker

--- lib/Noembed/Worker.pm
+++ lib/Noembed/Worker.pm
@@ -1,18 +1,27 @@
 package Noembed::Worker;
 
 use Noembed::Pygmentize;
-use Noembed::Imager;
+use Imager;
 
 our $pyg = Noembed::Pygmentize->new;
 
 sub run {
   my $job = shift;
+
   if ($job eq "dimensions") {
-    return Noembed::Imager::dimensions(@_);
+    my ($width, $height);
+    eval {
+      my $image = Imager->new(data => $data);
+      $width = $image->getwidth;
+      $height = $image->getheight;
+    };
+    return ($width, $height);
   }
+
   elsif ($job eq "colorize") {
     return $pyg->colorize(@_);
   }
+
   die "did not recognize job name";
 }
 
