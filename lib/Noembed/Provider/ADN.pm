13c5a228ca38139a51de66ee3c70eb35b0258c77
Lee Aylward <lee@laylward.com>
2013-06-03 19:23:28 -0400

    fix ADN provider and add icon

--- lib/Noembed/Provider/ADN.pm
+++ lib/Noembed/Provider/ADN.pm
@@ -3,7 +3,7 @@ package Noembed::Provider::ADN;
 use parent 'Noembed::oEmbedProvider';
 
 sub provider_name {"ADN"}
-sub patterns {'https://(alpha|posts|photos)\.app\.net/.*'}
-sub oembed_url {'https://https://alpha-api.app.net/oembed'}
+sub patterns {'https?://(alpha|posts|photos)\.app\.net/.*'}
+sub oembed_url {'https://alpha-api.app.net/oembed'}
 
 1;
