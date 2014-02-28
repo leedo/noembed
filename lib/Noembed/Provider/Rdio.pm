02e419cd2c47bdea4ca5a5e53624d1f6244036e8
Arthur Darcet <arthur.darcet@m4x.org>
2014-02-28 11:13:56 +0100

    Update the rdio provider

--- lib/Noembed/Provider/Rdio.pm
+++ lib/Noembed/Provider/Rdio.pm
@@ -5,9 +5,9 @@ use parent 'Noembed::oEmbedProvider';
 sub provider_name { "Rdio" }
 
 sub patterns {
-  'http://www\.rdio\.com/#/artist/[^/]+/album/[^/]+/?',
-  'http://www\.rdio\.com/#/artist/[^/]+/album/[^/]+/track/[^/]+/?',
-  'http://www\.rdio\.com/#/people/[^/]+/playlists/\d+/[^/]+',
+  'http://www\.rdio\.com/artist/[^/]+/album/[^/]+/?',
+  'http://www\.rdio\.com/artist/[^/]+/album/[^/]+/track/[^/]+/?',
+  'http://www\.rdio\.com/people/[^/]+/playlists/\d+/[^/]+',
 }
 
 sub oembed_url { "http://www.rdio.com/api/oembed/" }
