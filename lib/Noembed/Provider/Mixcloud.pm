17d9cfc8101fe60ea099ffef0c3f943d7cd4e922
leonardo <leofiore@gmail.com>
2016-04-15 09:41:03 -0400

    added mixcloud provider

---
+++ lib/Noembed/Provider/Mixcloud.pm
@@ -0,0 +1,13 @@
+package Noembed::Provider::Mixcloud;
+
+use parent 'Noembed::oEmbedProvider';
+
+sub provider_name {"Mixcloud"}
+sub patterns {'https?://(?:www\.)?mixcloud\.com/(.+)'}
+sub build_url {
+  my ($self, $req) = @_;
+  my $url = $req->captures[0];
+  return "https://www.mixcloud.com/oembed/?url=https://www.mixcloud.com/$url&format=json";
+}
+
+1;
