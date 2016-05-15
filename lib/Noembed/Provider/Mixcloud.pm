980fa1337a22f2389b489cfffa3c47c40b4a6183
Lee Aylward <lee@laylward.com>
2016-05-15 23:46:48 +0000

    syntax error

--- lib/Noembed/Provider/Mixcloud.pm
+++ lib/Noembed/Provider/Mixcloud.pm
@@ -6,7 +6,7 @@ sub provider_name {"Mixcloud"}
 sub patterns {'https?://(?:www\.)?mixcloud\.com/(.+)'}
 sub build_url {
   my ($self, $req) = @_;
-  my $url = $req->captures[0];
+  my $url = $req->captures->[0];
   return "https://www.mixcloud.com/oembed/?url=https://www.mixcloud.com/$url&format=json";
 }
 
