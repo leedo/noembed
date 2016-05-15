6a3b5d9ba786dff34f78cdc794f5e555061c1d94
Lee Aylward <lee@laylward.com>
2016-05-15 23:54:02 +0000

    switch TED to plain oembed

--- lib/Noembed/Provider/TED.pm
+++ lib/Noembed/Provider/TED.pm
@@ -1,33 +1,9 @@
 package Noembed::Provider::TED;
 
-use parent 'Noembed::Provider';
-
-use Web::Scraper;
-
-sub prepare_provider {
-  my $self = shift;
-
-  $self->{scraper} = scraper {
-    process 'meta[property="og:title"]', title => '@content';
-    process 'meta[property="og:description"]', description => '@description';
-    process 'meta[property="og:image"]', image => '@content';
-  };
-}
+use parent 'Noembed::oEmbedProvider';
 
 sub provider_name { "TED" }
-sub patterns { 'http://www\.ted\.com/talks/.+\.html' }
-
-sub serialize {
-  my ($self, $body, $req) = @_;
-
-  my $data = $self->{scraper}->scrape($body);
-  $data->{url} = $req->url;
-  $data->{url} =~ s/www\.ted\.com/embed\.ted\.com/;
-
-  return {
-    title => $data->{title},
-    html  => $self->render($data),
-  }
-}
+sub patterns { 'https?://www\.ted\.com/talks/.+' }
+sub oembed_url { 'https://www.ted.com/talks/oembed.json' }
 
 1;
