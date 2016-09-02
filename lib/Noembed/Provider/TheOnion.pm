2a50dc70829e023c0fabc27a7788891f6ad81a94
Lee Aylward <lee@laylward.com>
2016-09-02 14:35:27 +0000

    more flexible twitter URL

--- lib/Noembed/Provider/TheOnion.pm
+++ lib/Noembed/Provider/TheOnion.pm
@@ -3,6 +3,6 @@ package Noembed::Provider::TheOnion;
 use parent 'Noembed::TwitterCardProvider';
 
 sub provider_name { "The Onion" }
-sub patterns { 'http://www\.theonion\.com/articles/[^/]+/?' }
+sub patterns { 'http://www\.theonion\.com/articles?/[^/]+/?' }
 
 1;
--- lib/Noembed/Provider/Twitter.pm
+++ lib/Noembed/Provider/Twitter.pm
@@ -2,7 +2,15 @@ package Noembed::Provider::Twitter;
 
 use parent 'Noembed::oEmbedProvider';
 
-sub patterns { 'https?://(?:www|mobile\.)?twitter\.com/(?:#!/)?[^/]+/status(?:es)?/(\d+)' }
+sub patterns { 'https?://(?:www|mobile\.)?twitter\.com/(?:#!/)?([^/]+)/status(?:es)?/(\d+)' }
+
+sub build_url {
+  my ($self, $req) = @_;
+  my $captures = $req->captures;
+  $req->url(sprintf "https://twitter.com/%s/status/%s", @$captures);
+  $self->SUPER::build_url($req);
+}
+
 sub provider_name { "Twitter" }
 sub oembed_url { "https://publish.twitter.com/oembed" }
 
