04055d12ad54fb5e93336efade7d9b1450815257
Lee Aylward <lee@laylward.com>
2016-09-28 02:52:12 +0000

    switch imgur to oembed

--- lib/Noembed/Provider/Imgur.pm
+++ lib/Noembed/Provider/Imgur.pm
@@ -1,22 +1,9 @@
 package Noembed::Provider::Imgur;
 
-use Web::Scraper;
+use parent 'Noembed::oEmbedProvider';
 
-use parent 'Noembed::ImageProvider';
-
-sub prepare_provider {
-  my $self = shift;
-  $self->{scraper} = scraper {
-    process 'link[rel="image_src"]', src => '@href';
-  };
-}
-
-sub image_data {
-  my ($self, $body) = @_;
-  $self->{scraper}->scrape($body);
-}
-
-sub patterns { 'http://imgur\.com/([0-9a-zA-Z]+)$' }
+sub patterns { 'https?://imgur\.com/(?:[^\/]+/)?[0-9a-zA-Z]+$' }
 sub provider_name { "Imgur" }
+sub oembed_url { "http://api.imgur.com/oembed.json" }
 
 1;
