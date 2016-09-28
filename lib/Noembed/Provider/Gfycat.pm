de3050d27112eb4f2501e7d43b5b4bde40263f8e
Lee Aylward <lee@laylward.com>
2016-09-28 02:51:31 +0000

    switch gfycat to oembed

--- lib/Noembed/Provider/Gfycat.pm
+++ lib/Noembed/Provider/Gfycat.pm
@@ -1,8 +1,9 @@
 package Noembed::Provider::Gfycat;
 
-use parent 'Noembed::TwitterCardProvider';
+use parent 'Noembed::oEmbedProvider';
 
 sub provider_name { "Gfycat" }
 sub patterns { 'http://gfycat\.com/([a-zA-Z]+)' }
+sub oembed_url { 'https://api.gfycat.com/v1/oembed' }
 
 1;
