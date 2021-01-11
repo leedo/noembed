cb3b6c8b0fe4cb73a49454c9cea2dbe6eac72a81
Neil <neilsemail@me.com>
2021-01-11 13:53:31 -0500

    Specify HTTPS without &scheme

--- lib/Noembed/Provider/YouTube.pm
+++ lib/Noembed/Provider/YouTube.pm
@@ -15,11 +15,10 @@ sub options { qw/maxwidth maxheight autoplay/}
 
 sub build_url {
   my ($self, $req) = @_;
-  my $uri = URI->new("http://www.youtube.com/oembed/");
+  my $uri = URI->new("https://www.youtube.com/oembed/");
 
   my $id = $req->captures->[0];
   $uri->query_param("url", "http://www.youtube.com/watch?v=$id");
-  $uri->query_param("scheme", "https");
 
   return $uri;
 }
